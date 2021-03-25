13984e2148bfe0d62365dfe9186cca00c2f0942a
Thamires Lopes <thamiresl686@gmail.com>
2021-03-24 21:44:10 -0300

    Basic header

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Header from './components/header';
 import LoginPage from './pages/login';
 import SignUpPage from './pages/signUp';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route exact path="/login" component={LoginPage} />
-      <Route exact path="/signup" component={SignUpPage} />
+  <div>
+    <Header />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignUpPage} />
 
-    </Switch>
-  </BrowserRouter>
+      </Switch>
+    </BrowserRouter>
+  </div>
+
 );
 
 export default Routes;
